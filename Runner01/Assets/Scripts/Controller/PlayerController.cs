89246abdc3374106fcd6c7110ad9e0ca649a3c1a
21DP4RKaln <103189045+21DP4RKaln@users.noreply.github.com>
2023-09-12 18:46:05 +0300

    Initial commit

---
+++ Runner01/Assets/Scripts/Controller/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private CharacterController controller;
+    private Vector3 moveVector;
+    private float speed = 5.0f;
+    private float VerticalVelocity = 0.0f;
+    private float gravity = 12.0f;
+    public int score = 0;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        moveVector = Vector3.zero;
+        if (controller.isGrounded)
+        {
+            VerticalVelocity = -0.5f;
+        }
+        else
+        {
+            VerticalVelocity -= gravity * Time.deltaTime;
+        }
+
+        moveVector.x = Input.GetAxis("Horizontal") * speed;
+
+        moveVector.y = VerticalVelocity;
+
+        moveVector.z = speed;
+        controller.Move(moveVector * Time.deltaTime);
+    }
+    public void SetSpeed(float modifier)
+    {
+        speed = 5.0f + modifier;
+    }
+    public void AddScore(int value)
+    {
+        score += value;
+        UnityEngine.Debug.Log("Score: " + score); // Можно добавить вывод текущего количества очков в консоль или интерфейс игры
+    }
+
+}
---
+++ Runner01/Assets/Scripts/Controller/ScoreTextController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreTextController : MonoBehaviour
+{
+    public int score = 0;
+    public Text scoreText; // Ссылка на текстовый элемент
+
+    // Вызывается при подборе предмета, чтобы добавить очки
+    public void AddScore(int value)
+    {
+        score += value;
+        UpdateScoreText();
+    }
+
+    // Обновляет текстовый элемент с текущим количеством очков
+    private void UpdateScoreText()
+    {
+        scoreText.text = "Score: " + score.ToString();
+    }
+}
